a2e4301b0f34f34f17b9187082190d388cb6951c
Giridhar Kaushik <giridharkaushikr@gmail.com>
2020-02-25 13:45:04 -0500

    data and draft1 code

---
+++ senticlassFFNN.py
@@ -0,0 +1,196 @@
+import pandas as pd
+from collections import defaultdict
+from pathlib import Path
+import nltk as nl
+from nltk.tokenize import word_tokenize
+import re
+from nltk.tokenize.casual import TweetTokenizer
+import numpy as np
+import math
+import sys
+import operator
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+#nl.download('punkt')
+
+# pass path as train and test folder
+
+#stdoutorigin = sys.stdout
+#sys.stdout = open("D:\\Spring 2020\\assignments\\sentiment_classification\\log.txt", "w")
+################ Functions######################
+def get_data(path):
+    pospath = path + '/positive'
+    # create list to store text
+    results = defaultdict(list)
+
+    # loop through files and append text to list
+    for file in Path(pospath).iterdir():
+        with open(file, "r", encoding="utf8") as file_open:
+            results["text"].append(file_open.read())
+
+    # read the list in as a dataframe
+    df_pos = pd.DataFrame(results)
+
+    #set directory path
+    my_dir_path_neg = path + '/negative'
+
+    # create list to store text
+    results_neg = defaultdict(list)
+
+    # loop through files and append text to list
+    for file in Path(my_dir_path_neg).iterdir():
+        with open(file, "r", encoding="utf8") as file_open:
+            results_neg["text"].append(file_open.read())
+    # read the list in as a dataframe
+    df_neg = pd.DataFrame(results_neg)
+    df_neg.head()
+
+    #add sentiment to both datasets and then combine them for test data 1 for positive and 0 for negative
+    df_pos['Sentiment']=1
+    df_neg['Sentiment']=0
+    frames = [df_pos, df_neg]
+    df = pd.concat(frames)
+
+    # increase column width to see more of the tweets
+    pd.set_option('max_colwidth', 140)
+
+    # reshuffle the tweets to see both pos and neg in random order
+    df = df.sample(frac=1).reset_index(drop=True)
+
+    # explore top 5 rows
+    df.head(5)
+    return df
+
+def clean(df):
+
+    # Remove any markup tags (HTML), all the mentions of handles(starts with '@') and '#' character
+    def cleantweettext(raw_html):
+        pattern = re.compile('<.*?>')
+        cleantext = re.sub(pattern, '', raw_html)
+        cleantext = " ".join(filter(lambda x:x[0]!='@', cleantext.split()))
+        cleantext = cleantext.replace('#', '')
+        return cleantext
+
+    def removeat(text):
+        atlist=[]
+        for word in text:
+            pattern = re.compile('^@')
+            if re.match(pattern,word):
+                #cleantext1 = re.sub(pattern, word[1:], word)
+                atlist.append(word[1:])
+            else:
+                atlist.append(word)
+        return atlist
+
+    def tolower(text):
+        lowerlist=[]
+        for word in text:
+            pattern = re.compile('[A-Z][a-z]+')
+            if re.match(pattern,word):
+                cleantext1 = re.sub(pattern, word.lower(), word)
+                lowerlist.append(cleantext1)
+            else:
+                lowerlist.append(word)
+        return lowerlist
+
+    cleantweet= []
+    for doc in df.text:
+        cleantweet.append(cleantweettext(doc))
+
+
+    tokentweet=[]
+    df.text= cleantweet
+    for doc in df.text:
+        tokentweet.append(TweetTokenizer().tokenize(doc))
+    df.text= tokentweet
+
+    removeattweet=[]
+    for doc in df.text:
+        removeattweet.append(removeat(doc))
+    df.text =removeattweet
+
+    lowertweet=[]
+    for doc in df.text:
+        lowertweet.append(tolower(doc))
+    df.text = lowertweet
+
+    tweets=[]
+    for x in df.text:
+        tweet = ''
+        for word in x:
+            tweet += word+' '
+        tweets.append(word_tokenize(tweet))
+    df.text= tweets
+
+    #stemming
+    stemtweets=[]
+    from nltk.stem.snowball import SnowballStemmer
+    stemmer = SnowballStemmer("english", ignore_stopwords=False)
+    #ps= PorterStemmer()
+    for x in df.text:
+        stemtweet=''
+        for word in x:
+            stemtweet=stemtweet+stemmer.stem(word)+' '
+        stemtweets.append(word_tokenize(stemtweet))
+    df['stemmed']=stemtweets
+
+    df_unstemmed = pd.DataFrame()
+    df_unstemmed['text'] = df['text']
+    df_unstemmed['Sentiment'] = df['Sentiment']
+    df_stemmed = pd.DataFrame()
+    df_stemmed['text'] = df['stemmed']
+    df_stemmed['Sentiment'] = df['Sentiment']
+    
+    ### Finalize both the stemmed and unstemmed dataframes
+    #df_unstemmed = df.drop(['stemmed'], axis=1)
+    #df_unstemmed.head()
+
+    # create a df with stemmed text
+    #df_stemmed = df.drop(['text'], axis=1)
+    
+    return df_stemmed,df_unstemmed
+
+
+# initialize count vectorizer
+def dummy_fun(doc):
+    return doc
+
+def getrep(df, rep):
+    if rep == 'binary':
+        vectorizer = CountVectorizer(binary = True, analyzer='word', tokenizer=dummy_fun, preprocessor=dummy_fun, token_pattern=None)  
+    elif rep == 'freq': #for freq
+        vectorizer = CountVectorizer(analyzer='word', tokenizer=dummy_fun, preprocessor=dummy_fun, token_pattern=None)          
+    elif rep == 'tfidf':
+        vectorizer = TfidfVectorizer(analyzer='word', tokenizer=dummy_fun, preprocessor=dummy_fun, token_pattern=None)
+    text = df.text
+    vectorizer.fit(text)
+    freqVocab = vectorizer.vocabulary_
+    train_vector = vectorizer.transform(text)
+    #Create bigdoc that contains words in V, their corresponding frequencies for each class
+
+    #1.Transform pos and neg tweets into seprate vectors
+    train_pos_vector1 = vectorizer.transform(df[df['Sentiment']==1]['text'])
+    train_neg_vector1 = vectorizer.transform(df[df['Sentiment']==0]['text'])
+
+    #2. column sum of vectors(word per column)
+    sum_pos = train_pos_vector1.sum(axis = 0)
+    sum_neg = train_neg_vector1.sum(axis = 0)
+
+    #3. Initialize bigdoc as a dataframe
+    bigdoc = pd.DataFrame(index = list(set(freqVocab.keys())), columns = ['pos', 'neg'])
+
+    #4. get the corresponding frequency from the above matrx and set it to bigdoc
+    for word in freqVocab.keys():
+        index = freqVocab.get(word)
+        bigdoc.at[word, 'pos'] = sum_pos[:, index].item()
+        bigdoc.at[word, 'neg'] = sum_neg[:, index].item()
+#here
+    #print("length of vocab: ")
+    #print(len(freqVocab))
+    return bigdoc, freqVocab, train_vector, vectorizer
