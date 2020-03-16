8c85a0743599d26fcb4705628f3542e972c02f58
harithagmu <43484154+harithagmu@users.noreply.github.com>
2020-03-15 21:42:35 -0400

    Part2 - modified training and Added testing

--- LanguageModel.py
+++ LanguageModel.py
@@ -39,28 +39,21 @@ from collections import defaultdict
 from pathlib import Path
 import nltk as nl
 from nltk.tokenize import word_tokenize
-from nltk.util import ngrams
 import re
 from nltk.tokenize.casual import TweetTokenizer
 import numpy as np
-import math
 import sys
-import operator
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import OneHotEncoder
-import random
+from nltk.stem.snowball import SnowballStemmer
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
+from torch.autograd import Variable
+from sklearn import preprocessing
 import time
+from sklearn.model_selection import StratifiedKFold
+nl.download('punkt')
+
 
 start_time = time.time()
 
@@ -143,7 +136,6 @@ def clean(df):
 
     #stemming
     stemtweets=[]
-    from nltk.stem.snowball import SnowballStemmer
     stemmer = SnowballStemmer("english", ignore_stopwords=False)
     #ps= PorterStemmer()
     for x in df.text:
@@ -159,14 +151,7 @@ def clean(df):
     df_stemmed = pd.DataFrame()
     df_stemmed['text'] = df['stemmed']
     df_stemmed['Sentiment'] = df['Sentiment']
-    
-    ### Finalize both the stemmed and unstemmed dataframes
-    #df_unstemmed = df.drop(['stemmed'], axis=1)
-    #df_unstemmed.head()
-
-    # create a df with stemmed text
-    #df_stemmed = df.drop(['text'], axis=1)
-    
+   
     return df_stemmed,df_unstemmed
 
 
@@ -184,7 +169,7 @@ def pos_sample_bigrammer(df):
 	bigrams=[]
 	bigramlist=[]
 	for i in range(len(wordsinorder)-1):
-		bigram_list=[wordsinorder[i], wordsinorder[i + 1]]
+		bigram_list=[wordsinorder[i], wordsinorder[i + 1], 1]
 		#print(bigram_list)
 		bigrams.append(bigram_list)
 	#print(bigrams)
@@ -215,54 +200,53 @@ def neg_sample_bigrammer(bigramlist,vocab,k):
 			num= vocab.index(word)
 			neglist= random.sample([i for i in range(0,end) if i not in [num]],k)
 			for j in neglist:
-				negsample= [posbigram[0][0],vocab[j]]
+				negsample= [posbigram[0][0], vocab[j], 0]
 				negbigsample.append(negsample)
 	print(len(negbigsample))
 	return negbigsample
 
-class NGramLanguageModeler(nn.Module):
 
-	    def __init__(self, vocab_size, embedding_dim, context_size):
-	        super(NGramLanguageModeler, self).__init__()
-	        self.embeddings = nn.Embedding(vocab_size, embedding_dim)
-	        self.linear1 = nn.Linear(context_size * embedding_dim, 20)
-	        self.linear2 = nn.Linear(20, vocab_size)
+class SkipGram(torch.nn.Module):
+    def __init__(self, vocab_size, embedding_dim):
+        super(SkipGram, self).__init__()
+        self.embeddings_u = torch.nn.Embedding(vocab_size, embedding_dim)
+        self.embeddings_v = torch.nn.Embedding(vocab_size, embedding_dim)
 
-	    def forward(self, inputs):
-	        embeds = self.embeddings(inputs).view((1, -1))
-	        out = torch.sigmoid(self.linear1(embeds))
-	        out = self.linear2(out)
-	        log_probs = torch.sigmoid(out)
-	        return log_probs
-
-
-def Languagemodel(bigrams_data, labels,vocab2,d):
+    def forward(self, word, context) -> torch.Tensor:
+        emb_word = self.embeddings_u(word).view((1, -1))
+        emb_cont = self.embeddings_v(context).view((1, -1))
+        score = torch.mm(emb_word, torch.t(emb_cont))
+        output = torch.nn.functional.logsigmoid(score)
+        return output
+    
+def Languagemodel(bigrams_data, vocab2,d):
     bigram= bigrams_data
     print(bigram[1])
     vocab = set(vocab2)
     word_to_ix = {word: i for i, word in enumerate(vocab)}
     losses = []
-    loss_function = torch.nn.CrossEntropyLoss()
-    model = NGramLanguageModeler(len(vocab), d, 1)
+    loss_function = torch.nn.MSELoss()
+    model = SkipGram(len(vocab), d)
     optimizer = optim.SGD(model.parameters(), lr=0.0001)
-    for epoch in range(1):
+    for epoch in range(200):
         total_loss = 0
-        for context, target in bigram:
+        for context, target, label in bigram:
            # Step 1. Prepare the inputs to be passed to the model (i.e, turn the words
            # into integer indices and wrap them in tensors)
             if context in vocab2:
                 context_idxs = torch.tensor(word_to_ix[context], dtype=torch.long) 
+                target_idxs = torch.tensor(word_to_ix[target], dtype=torch.long) 
                 
                 #step 2. Recall that torch *accumulates* gradients. Before passing in a
                 # new instance, you need to zero out the gradients from the old instance
                 model.zero_grad()
                 
                 # Step 3. Run the forward pass, getting log probabilities over next words
-                log_probs = model(context_idxs)
+                log_probs = model(context_idxs, target_idxs)
                 
                 # Step 4. Compute your loss function. (Again, Torch wants the target
                 # word wrapped in a tensor)
-                loss = loss_function(log_probs, torch.tensor(word_to_ix[target], dtype=torch.long))
+                loss = loss_function(log_probs[0], torch.tensor([label], dtype=torch.float))
                 
                 # Step 5. Do the backward pass and update the gradient
                 loss.backward()
@@ -273,13 +257,27 @@ def Languagemodel(bigrams_data, labels,vocab2,d):
         losses.append(total_loss)
         plt.title("plot of training loss ")
         plt.plot(losses)
-                
 
 
-import os
+def Testing(model, vocab, data):
+    predictions = []
+    actuals = []
+    word_to_ix = {word: i for i, word in enumerate(vocab)}
+    for context, target, label in data:
+        context_idxs = torch.tensor(word_to_ix[context], dtype=torch.long) 
+        target_idxs = torch.tensor(word_to_ix[target], dtype=torch.long) 
+        model.eval()
+        log_probs = model(context_idxs, target_idxs)
+        _, prediction = torch.max(log_probs.data, 1)
+        prediction = prediction.data[0]
+        predictions.append(prediction)
+        actuals.append(label)
+    print(confusion_matrix(actuals, predictions))
+    
+
 
 def main():
-
+    import os
     os.chdir("D:\\Spring 2020\\assignments\\senticlassFFNL-master\\senticlassFFNL")
     # print command line arguments
     train= get_data("tweet\\train") #get_data(sys.argv[1])
@@ -305,30 +303,32 @@ def main():
     train_nostem_pos_bigram= pos_sample_bigrammer(clean_train_nostem)
     
     print("positive samples for training created")
-    print("No of no stem pos bigrams=", len(train_nostem_pos_bigram))
     print("No of stem pos bigrams=" ,len(train_stem_pos_bigram))
+    print("No of no stem pos bigrams=", len(train_nostem_pos_bigram))
+    
 
     print("creating negative sample bigrams")
     train_stem_neg_bigram = neg_sample_bigrammer(train_stem_pos_bigram, Vocab_stem,2)
     train_nostem_neg_bigram = neg_sample_bigrammer(train_nostem_pos_bigram, Vocab_nostem,2)
     print("negative samples created")
     
-    print("No of no stem neg bigrams=", len(train_nostem_neg_bigram))
     print("No of stem neg bigrams=" ,len(train_stem_neg_bigram))
+    print("No of no stem neg bigrams=", len(train_nostem_neg_bigram))
+ 
 
     #create a training dataframe with positive and negative samples and adding labels(1,0)  for them
     train_stem_data = train_stem_pos_bigram + train_stem_neg_bigram
     train_nostem_data= train_nostem_pos_bigram + train_nostem_neg_bigram
     
-    nostem_labels =[0]*len(train_nostem_pos_bigram) + [1]*len(train_nostem_neg_bigram)
-    stem_labels= [0]*len(train_stem_pos_bigram)+ [1]*len(train_stem_neg_bigram)
+    #nostem_labels =[0]*len(train_nostem_pos_bigram) + [1]*len(train_nostem_neg_bigram)
+    #stem_labels= [0]*len(train_stem_pos_bigram)+ [1]*len(train_stem_neg_bigram)
 
     print("--- %s seconds ---" %(time.time() - start_time))
     print("train data is ready for stem and no stem ")
 
     print("Creating Language model")
-    Languagemodel(train_stem_data[:10], stem_labels[:10], Vocab_stem, 10)
-    Languagemodel(train_nostem_data, nostem_labels, Vocab_stem, 10)
+    stem_model =  Languagemodel(train_stem_data, Vocab_stem, 10)
+    Languagemodel(train_nostem_data, Vocab_stem, 10)
     
     print("*********TESTING************")
     print("creating positive bigrams")
@@ -353,8 +353,12 @@ def main():
     print("No of stem neg bigrams=" ,len(test_stem_neg_bigram))
 
     #Create labels
-    train_stem_data = test_stem_pos_bigram + test_stem_neg_bigram
-    train_nostem_data= test_nostem_pos_bigram + test_nostem_neg_bigram
-
+    test_stem_data = test_stem_pos_bigram + test_stem_neg_bigram
+    test_nostem_data= test_nostem_pos_bigram + test_nostem_neg_bigram
+    
+    #Testing
+    Testing(stem_model, Vocab_stem_test, test_stem_data)
+    Testing(stem_model, Vocab_nostem_test, test_nostem_data)
+    
 if __name__ == "__main__":
     main()
